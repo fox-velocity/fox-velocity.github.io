b765a069abfb359f953cb85a7771abc66af3cd4a
FoxVelocity <foxvelocity@laposte.net>
2025-02-10 23:40:43 +0100

    Update constants.js

--- js/modules/constants.js
+++ js/modules/constants.js
@@ -89,7 +89,7 @@ export const exchangeToCurrency = {
     'BATS': 'USD',
     'CBOE': 'USD',
     'IEX': 'USD',
- 'PARIS': 'EUR',
+ 'Paris': 'EUR',
     'PAR': 'EUR',
     'FRA': 'EUR',
     'GER': 'EUR',
